b1f51d5a2b0c482918e22ece2345a09f6dee4bb9
Chris Ihli <ihliman@gmail.com>
2021-05-05 00:52:25 -0700

    Added routes added views added new Article page with form and bootstrap styling

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const articleRouter = require('./routes/articles');
 const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use('/articles', articleRouter)
+
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    const articles = [{
+            title: 'Test Article',
+            createdAt: new Date(),
+            description: 'Test description',
+        },
+        {
+            title: 'Test Article2',
+            createdAt: new Date(),
+            description: 'Test description2',
+        }
+    ];
+    res.render('articles/index', { articles: articles });
 });
 app.listen(5000);
